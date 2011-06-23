14f93b0a2f862a168845239da963a34b4d7b8569
Dave Yeu <daveyeu@gmail.com>
2011-06-23 17:12:33 -0400

    Don't forget to restart TLS after reconnects

--- lib/em-apn/client.rb
+++ lib/em-apn/client.rb
@@ -4,7 +4,6 @@ module EventMachine
       SANDBOX_GATEWAY    = "gateway.sandbox.push.apple.com"
       PRODUCTION_GATEWAY = "gateway.push.apple.com"
       PORT = 2195
-      MAX_RETRIES = 10
 
       # Create a connection to Apple's push notification gateway
       #
@@ -32,6 +31,8 @@ module EventMachine
         @port = options[:port]
         @key  = options[:key]  || ENV["APN_KEY"]
         @cert = options[:cert] || ENV["APN_CERT"]
+
+        @reconnect_retries = 0
       end
 
       def deliver(notification)
@@ -49,6 +50,16 @@ module EventMachine
 
       def connection_completed
         EM::APN.logger.info("Connection established")
+
+        if @reconnect_retries > 0
+          EM::APN.logger.info("Reconnect detected... re-starting TLS")
+          start_tls(
+            :private_key_file => @key,
+            :cert_chain_file  => @cert,
+            :verify_peer      => false
+          )
+          @reconnect_retries = 0
+        end
       end
 
       def post_init
@@ -68,24 +79,42 @@ module EventMachine
         end
       end
 
-      # If the connection drops, attempt to reconnect with exponential decay
+      # Crazy crazy crazy
+      #
+      # Apple severs the connection if there are any errors (bad token and the like).
+      # This will immediately attempt to reconnect, and if that fails, it will start
+      # to back off exponentially with an upper limit of sixty seconds between attempts.
+      #
+      # And it will keep trying forever and ever.
+      #
+      # Note also that #post_init isn't called as a result of #reconnect, so the TLS
+      # setup occurs inside of #connection_completed for these retries. Ugh.
+      #
+      # Future work may try to disentangle all this by simply letting the connection
+      # die and re-connecting with a brand new client inside of EM::APN.push.
+      # Queuing up behind a reconnect at that level is probably preferable since any
+      # data that is sent out while this thing is reconnecting is simply dropped.
       def unbind
         EM::APN.logger.info("Connection closed")
 
         # Wrap up retry logic in next_tick so that this doesn't fire if the event loop
         # itself is being closed (normal exit).
         EM.next_tick do
-          @reconnect_retries ||= 0
+          @reconnect_retries += 1
 
-          if @reconnect_retries >= MAX_RETRIES
-            EM::APN.logger.error("Max retries exceeded (#{MAX_RETRIES})... giving up!")
+          # If this is the first retry, attempt to reconnect immediately.
+          # Otherwise, keep retrying on an exponential decay
+          if @reconnect_retries == 1
+            EM::APN.logger.warn("Reconnecting to #{@host} (attempt #{@reconnect_retries})")
+            reconnect(@host, @port)
           else
-            retry_interval = 2 ** @reconnect_retries
-            @reconnect_retries += 1
+            retry_interval = 2 ** (@reconnect_retries - 2)
+            retry_interval = 60 if retry_interval > 60
+
             EM::APN.logger.warn("Reconnecting in #{retry_interval} seconds...")
 
             EM.add_timer(retry_interval) do
-              EM::APN.logger.warn("Reconnecting to #{@host}")
+              EM::APN.logger.warn("Reconnecting to #{@host} (attempt #{@reconnect_retries})")
               reconnect(@host, @port)
             end
           end
